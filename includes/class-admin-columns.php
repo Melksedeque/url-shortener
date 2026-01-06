ab3330982306c0041f17758159363f0135494c7a
Melksedeque <freelancer@melksedeque.com.br>
2026-01-05 22:12:26 -0300

    Arquivo de classe responsável pelas colunas novas na exibição dos posts com a url curta

---
+++ includes/class-admin-columns.php
@@ -0,0 +1,141 @@
+<?php
+namespace WP_URL_Shortener;
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class Admin_Columns {
+    
+    public function init() {
+        // Adiciona colunas para post types
+        $enabled_post_types = get_option('wpus_enabled_post_types', []);
+        foreach ($enabled_post_types as $post_type) {
+            add_filter("manage_{$post_type}_posts_columns", [$this, 'add_post_column']);
+            add_action("manage_{$post_type}_posts_custom_column", [$this, 'render_post_column'], 10, 2);
+        }
+
+        // Adiciona colunas para taxonomias
+        $enabled_taxonomies = get_option('wpus_enabled_taxonomies', []);
+        foreach ($enabled_taxonomies as $taxonomy) {
+            add_filter("manage_edit-{$taxonomy}_columns", [$this, 'add_term_column']);
+            add_filter("manage_{$taxonomy}_custom_column", [$this, 'render_term_column'], 10, 3);
+        }
+
+        // Enfileira scripts para copiar URL
+        add_action('admin_enqueue_scripts', [$this, 'enqueue_column_assets']);
+    }
+
+    public function add_post_column($columns) {
+        $new_columns = [];
+        
+        foreach ($columns as $key => $value) {
+            $new_columns[$key] = $value;
+            
+            // Adiciona a coluna após a coluna 'date'
+            if ($key === 'date') {
+                $new_columns['wpus_short_url'] = __('URL Curta', 'wp-url-shortener');
+            }
+        }
+        
+        return $new_columns;
+    }
+
+    public function render_post_column($column, $post_id) {
+        if ($column !== 'wpus_short_url') {
+            return;
+        }
+
+        $short_code = get_post_meta($post_id, '_wpus_short_code', true);
+        
+        if (empty($short_code)) {
+            echo '<span style="color: #999;">' . __('Não gerada', 'wp-url-shortener') . '</span>';
+            return;
+        }
+
+        $generator = new Shortcode_Generator();
+        $short_url = $generator->get_short_url($short_code);
+        
+        $this->render_url_with_copy($short_url, $short_code);
+    }
+
+    public function add_term_column($columns) {
+        $new_columns = [];
+        
+        foreach ($columns as $key => $value) {
+            $new_columns[$key] = $value;
+            
+            // Adiciona a coluna após a coluna 'slug'
+            if ($key === 'slug') {
+                $new_columns['wpus_short_url'] = __('URL Curta', 'wp-url-shortener');
+            }
+        }
+        
+        return $new_columns;
+    }
+
+    public function render_term_column($content, $column, $term_id) {
+        if ($column !== 'wpus_short_url') {
+            return $content;
+        }
+
+        $short_code = get_term_meta($term_id, '_wpus_short_code', true);
+        
+        if (empty($short_code)) {
+            return '<span style="color: #999;">' . __('Não gerada', 'wp-url-shortener') . '</span>';
+        }
+
+        $generator = new Shortcode_Generator();
+        $short_url = $generator->get_short_url($short_code);
+        
+        ob_start();
+        $this->render_url_with_copy($short_url, $short_code);
+        return ob_get_clean();
+    }
+
+    private function render_url_with_copy($short_url, $short_code) {
+        ?>
+        <div class="wpus-url-wrapper">
+            <code class="wpus-short-url"><?php echo esc_html($short_code); ?></code>
+            <button 
+                type="button" 
+                class="button button-small wpus-copy-btn" 
+                data-url="<?php echo esc_attr($short_url); ?>"
+                title="<?php esc_attr_e('Copiar URL', 'wp-url-shortener'); ?>"
+            >
+                <span class="dashicons dashicons-admin-page"></span>
+            </button>
+            <span class="wpus-copied-message" style="display: none;">
+                <?php _e('Copiado!', 'wp-url-shortener'); ?>
+            </span>
+        </div>
+        <?php
+    }
+
+    public function enqueue_column_assets($hook) {
+        // Carrega apenas nas páginas de listagem
+        if (!in_array($hook, ['edit.php', 'edit-tags.php'])) {
+            return;
+        }
+
+        wp_enqueue_style(
+            'wpus-columns-css',
+            WP_URL_SHORTENER_PLUGIN_URL . 'assets/css/columns.css',
+            [],
+            WP_URL_SHORTENER_VERSION
+        );
+
+        wp_enqueue_script(
+            'wpus-columns-js',
+            WP_URL_SHORTENER_PLUGIN_URL . 'assets/js/columns.js',
+            ['jquery'],
+            WP_URL_SHORTENER_VERSION,
+            true
+        );
+
+        wp_localize_script('wpus-columns-js', 'wpusColumns', [
+            'copiedText' => __('Copiado!', 'wp-url-shortener'),
+            'copyText' => __('Copiar URL', 'wp-url-shortener'),
+        ]);
+    }
+}
