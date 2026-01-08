369b0682096670294028e9f14706c0c787a0cece
Melksedeque <freelancer@melksedeque.com.br>
2026-01-08 16:30:25 -0300

    Corrigir includes/class-admin.php (Sanitização, Unslash, Escapamento)

--- includes/class-admin-columns.php
+++ includes/class-admin-columns.php
@@ -34,7 +34,7 @@ class Admin_Columns {
             
             // Adiciona a coluna após a coluna 'date'
             if ($key === 'date') {
-                $new_columns['wpus_short_url'] = __('URL Curta', 'url-shortener-by-melk');
+                $new_columns['wpus_short_url'] = esc_html__('URL Curta', 'url-shortener-by-melk');
             }
         }
         
@@ -49,7 +49,7 @@ class Admin_Columns {
         $short_code = get_post_meta($post_id, '_wpus_short_code', true);
         
         if (empty($short_code)) {
-            echo '<span style="color: #999;">' . __('Não gerada', 'url-shortener-by-melk') . '</span>';
+            echo '<span style="color: #999;">' . esc_html__('Não gerada', 'url-shortener-by-melk') . '</span>';
             return;
         }
 
@@ -67,7 +67,7 @@ class Admin_Columns {
             
             // Adiciona a coluna após a coluna 'slug'
             if ($key === 'slug') {
-                $new_columns['wpus_short_url'] = __('URL Curta', 'url-shortener-by-melk');
+                $new_columns['wpus_short_url'] = esc_html__('URL Curta', 'url-shortener-by-melk');
             }
         }
         
@@ -82,7 +82,7 @@ class Admin_Columns {
         $short_code = get_term_meta($term_id, '_wpus_short_code', true);
         
         if (empty($short_code)) {
-            return '<span style="color: #999;">' . __('Não gerada', 'url-shortener-by-melk') . '</span>';
+            return '<span style="color: #999;">' . esc_html__('Não gerada', 'url-shortener-by-melk') . '</span>';
         }
 
         $generator = new Shortcode_Generator();
@@ -106,7 +106,7 @@ class Admin_Columns {
                 <span class="dashicons dashicons-admin-page"></span>
             </button>
             <span class="wpus-copied-message" style="display: none;">
-                <?php _e('Copiado!', 'url-shortener-by-melk'); ?>
+                <?php esc_html_e('Copiado!', 'url-shortener-by-melk'); ?>
             </span>
         </div>
         <?php
@@ -134,8 +134,8 @@ class Admin_Columns {
         );
 
         wp_localize_script('wpus-columns-js', 'wpusColumns', [
-            'copiedText' => __('Copiado!', 'url-shortener-by-melk'),
-            'copyText' => __('Copiar URL', 'url-shortener-by-melk'),
+            'copiedText' => esc_html__('Copiado!', 'url-shortener-by-melk'),
+            'copyText' => esc_html__('Copiar URL', 'url-shortener-by-melk'),
         ]);
     }
 }
